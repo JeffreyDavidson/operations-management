0f8d6409647e3afa350753852d6a6113a4ea8652
Jefffrey Davidson <jeffrey.davidson@outlook.com>
2019-11-01 13:46:04 -0500

    Modify location of compiled files

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,7 +11,7 @@ let mix = require('laravel-mix');
  |
  */
 
-mix.js('src/js/app.js', 'public/js').sass('src/css/app.scss', 'public/css');
+mix.js('src/js/app.js', 'public/assets/js').sass('src/css/app.scss', 'public/assets/css');
 
 // Full API
 // mix.js(src, output);
