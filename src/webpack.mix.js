51f68d7dec59350fc4f1f23204bb3a7f26ee985e
Christopher Hacia <christopher.hacia@walmartlabs.com>
2019-11-06 09:27:05 -0800

    minor tweaks

--- src/webpack.mix.js
+++ src/webpack.mix.js
@@ -11,7 +11,7 @@ let mix = require('laravel-mix');
  |
  */
 
-mix.js('src/assets/js/app.js', 'dist/js').sass('src/assets/css/app.scss', 'dist/css');
+mix.js('./assets/js/app.js', '../dist/assets/js').sass('./assets/css/app.scss', '../dist/assets/css');
 
 // Full API
 // mix.js(src, output);
